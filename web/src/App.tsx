b8a1f4d31bbf013666f799306ee920840ee1c69b
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-07-16 13:20:58 +0300

    Setup GraphQL Code Generator

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "@apollo/client";
 import React from "react";
+import { useHelloQuery } from "./generated/graphql";
 
 function App() {
-  const { data, loading } = useQuery(gql`
-    {
-      hello
-    }
-  `);
+  // const { data, loading } = useQuery(gql`
+  //   {
+  //     hello
+  //   }
+  // `);
+  const { data, loading } = useHelloQuery();
   if (loading) return <div>loading...</div>;
-  return <div>{JSON.stringify(data)}</div>;
+  return <div>{data?.hello}</div>;
 }
 
 export default App;
