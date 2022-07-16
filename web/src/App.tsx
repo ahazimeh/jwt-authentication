d0a1d2b72f18222a05f78acd79154531495b99b9
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-07-16 08:45:17 +0300

    Setup Apollo Boost

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,15 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "@apollo/client";
 import React from "react";
 
 function App() {
-  return <div>hello</div>;
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+  if (loading) return <div>loading...</div>;
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import ApolloClient from "apollo-boost";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// import { ApolloProvider } from "@apollo/react-hooks";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+// const client: any = new ApolloClient({
+//   uri: "http://localhost:4000/graphql",
+// });
+const client = new ApolloClient({
+  // uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
