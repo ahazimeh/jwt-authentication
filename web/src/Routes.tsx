3e643d63fe74eceee22c2caa378241c4a370df60
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-07-16 20:14:07 +0300

    Create Register page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>aaa</div>}>
-          {/* <Route index element={<Home />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route> */}
-        </Route>
-      </Routes>
+      <div>
+        <header>
+          <div>
+            <Link to="/">home</Link>
+          </div>
+          <div>
+            <Link to="/register">register</Link>
+          </div>
+          <div>
+            <Link to="/login">login</Link>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />x
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface loginProps {}
+
+export const Login: React.FC<loginProps> = ({}) => {
+  return <div>login</div>;
+};
